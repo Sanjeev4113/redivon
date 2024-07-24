b990d4197b3bc40fc26f6b83ab9be142f32d44f2
Sanjeev4113 <sanjeevsudarsanam@gmail.com>
2024-07-24 22:43:41 +0530

    yup

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,7 @@
 const dotenv = require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
 const cors = require("cors");
-const userRoute = require("./routes/userRoute");
-const productRoute = require("./routes/productRoute");
-const contactRoute = require("./routes/contactRoute");
-const errorHandler = require("./middleWare/errorMiddleware");
 const cookieParser = require("cookie-parser");
 const path = require("path");
 
@@ -15,38 +10,37 @@ const app = express();
 // Middlewares
 app.use(express.json());
 app.use(cookieParser());
-app.use(express.urlencoded({extended: false}));
-app.use(bodyParser.json());
 app.use(cors({
-    origin: ["http://localhost:3000","https://pinvent-app.vercel.app"],
-    credentials: true,
- }));
-
-app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+    origin: '*',
+    credentials: true
+  }));
+app.use(express.static(path.join(__dirname, "../frontend/build")));
 
 // Routes Middleware
+const userRoute = require("./routes/userRoute");
+const productRoute = require("./routes/productRoute");
+const contactRoute = require("./routes/contactRoute");
 app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
 app.use("/api/contactus", contactRoute);
 
 // Routes
 app.get("/", (req, res) => {
-    res.send("Home Page");
+    res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
 });
 
 // Error Middleware
+const errorHandler = require("./middleWare/errorMiddleware");
 app.use(errorHandler);
-
 
 const PORT = process.env.PORT || 5000;
 
 // Connect to DB and start server
 mongoose 
-    .connect(process.env.MONGO_URI)
-    .then(() => {
-
+   .connect(process.env.MONGO_URI)
+   .then(() => {
         app.listen(PORT, () => {
-            console.log(`Server Running on Port ${PORT}`)
+            console.log(`Server Running on Port ${PORT}`); 
         })
     })
-    .catch((err) => console.log(err))
+   .catch((err) => console.log(err))   
