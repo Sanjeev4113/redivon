ae9ca13b80fbb456d8e18456f702c75a96b4e68f
Sanjeev4113 <sanjeevsudarsanam@gmail.com>
2024-07-24 23:39:03 +0530

    ui

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
     origin: (origin, callback) => {
-      const allowedOrigins = ['https://inventory-app-3i8q.onrender.com', 'http://localhost:3000'];
+      const allowedOrigins = ['https://inventory-app-3i8q.onrender.com', 'http://localhost:3000','*'];
       if (allowedOrigins.includes(origin)) {
         callback(null, true);
       } else {
