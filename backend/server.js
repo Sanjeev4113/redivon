1f6775feb03310bdf6d7a22ecf3966e976b8c5b8
Sanjeev4113 <sanjeevsudarsanam@gmail.com>
2024-07-25 00:56:15 +0530

    sf

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: 'http://localhost:3000', // Allow all origins
+    origin: ['http://localhost:3000', 'https://inventory-app-3i8q.onrender.com'],// Allow all origins
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
